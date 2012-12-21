91db3e10157b26b81b9e070912bf82c74e4f6ed1
WinSuk <goodboy735@gmail.com>
2012-12-21 10:05:35 -0800

    Fix build error on ARMv6
    Change-Id: Idedf64ae64804cd68e2cdef20fa1d1b56798a0eb
    
    Conflicts:
    	native/jni/Android.mk

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -95,8 +95,11 @@ endif # FLAG_DO_PROFILE
 LOCAL_MODULE := libjni_latinime_common_static
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := stlport_static
+ifdef HISTORICAL_NDK_VERSIONS_ROOT # In the platform build system
+include external/stlport/libstlport.mk
+else # In the NDK build system
+LOCAL_C_INCLUDES += external/stlport/stlport bionic
+endif
 
 include $(BUILD_STATIC_LIBRARY)
 ######################################
@@ -118,9 +121,11 @@ endif # FLAG_DO_PROFILE
 LOCAL_MODULE := libjni_latinime
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := stlport_static
-LOCAL_LDFLAGS += -ldl
+<<<<<<< HEAD
+ifdef HISTORICAL_NDK_VERSIONS_ROOT # In the platform build system
+LOCAL_STATIC_LIBRARIES += libstlport_static
+endif
+
 
 include $(BUILD_SHARED_LIBRARY)
 
